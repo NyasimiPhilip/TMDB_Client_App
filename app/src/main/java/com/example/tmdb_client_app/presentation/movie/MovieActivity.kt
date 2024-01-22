c5b8b5a44a15e0b71bc098d66a8aeccd2b2d6de8
NyasimiPhilip <nyasimiphilip@gmail.com>
2024-01-22 15:21:08 +0300

    Update

--- app/src/main/java/com/example/tmdb_client_app/presentation/movie/MovieActivity.kt
+++ app/src/main/java/com/example/tmdb_client_app/presentation/movie/MovieActivity.kt
@@ -2,11 +2,14 @@ package com.example.tmdb_client_app.presentation.movie
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.tmdb_client_app.databinding.ActivityMovieBinding  // Make sure to import the correct binding class
 import com.example.tmdb_client_app.R
 import com.example.tmdb_client_app.data.model.movie.Movie
@@ -18,25 +21,46 @@ class MovieActivity : AppCompatActivity() {
     @Inject
     lateinit var factory: MovieViewModelFactory
     private lateinit var movieViewModel: MovieViewModel
+    private lateinit var adapter: MovieAdapter
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         // Initialize the binding object using the generated binding class
         binding = DataBindingUtil.setContentView(this, R.layout.activity_movie)
         (application as Injector).createMoviesSubComponent()
             .inject(this)
 
 
-        movieViewModel = ViewModelProvider(this, factory )
-            .get(MovieViewModel::class.java)
-        val responseLiveData: LiveData<List<Movie>?> = movieViewModel.getMovies()
+        movieViewModel = ViewModelProvider(this, factory )[MovieViewModel::class.java]
+
+        initRecyclerView()
+        /**
+         * val responseLiveData: LiveData<List<Movie>?> = movieViewModel.getMovies()
         responseLiveData.observe(this, Observer {
             Log.i("MYTAG", it.toString())
         })
+         */
+    }
+    private fun initRecyclerView(){
+        binding.movieRecyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = MovieAdapter()
+        binding.movieRecyclerView.adapter = adapter
+        displayPopularMovies()
+    }
 
-
-
-
-
+    private fun displayPopularMovies(){
+        binding.movieProgressBar.visibility = View.VISIBLE
+        val responseLiveData = movieViewModel.getMovies()
+        responseLiveData.observe(this, Observer {
+            if(it!=null){
+                adapter.setList(it)
+                adapter.notifyDataSetChanged()
+                binding.movieProgressBar.visibility = View.GONE
+            }else{
+                binding.movieProgressBar.visibility = View.GONE
+                Toast.makeText(applicationContext,"No data available", Toast.LENGTH_LONG).show()
+            }
+        })
     }
 }
