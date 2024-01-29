71e979272bb23a7019945a1a48518572af9a57b8
NyasimiPhilip <nyasimiphilip@gmail.com>
2024-01-29 12:13:24 +0300

    Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("kotlin-kapt")
-
+    id ("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -47,6 +47,7 @@ android {
 
 dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.test.ext:junit-ktx:1.1.5")
     // Room
     val room_version = "2.6.1"
     implementation("androidx.room:room-runtime:$room_version")
@@ -65,10 +66,6 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:$lifecycle_version")
     kapt("androidx.lifecycle:lifecycle-compiler:$lifecycle_version")
 
-    // Dagger
-    val dagger_version = "2.50"
-    implementation("com.google.dagger:dagger:$dagger_version")
-    kapt("com.google.dagger:dagger-compiler:$dagger_version")
 
     // OkHttp
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
@@ -87,6 +84,18 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
 
+    //daggerHilt
+    implementation("com.google.dagger:hilt-android:2.48")
+    kapt("com.google.dagger:hilt-android-compiler:2.48")
+
+
+    // Testing
+    testImplementation("junit:junit:4.13.2")
+    testImplementation ("org.robolectric:robolectric:4.11.1")
+    testImplementation ("androidx.arch.core:core-testing:2.1.0")
+    testImplementation ("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.0-RC2")
+    testImplementation ("com.google.truth:truth:1.3.0")
+
     // Testing
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
@@ -97,7 +106,9 @@ dependencies {
 
     //data Binding
     implementation ("androidx.databinding:databinding-runtime:7.0.0")
-    testImplementation("com.google.truth.extensions:truth-java8-extension:1.3.0")
+}
 
-
+// Allow references to generated code
+kapt {
+    correctErrorTypes = true
 }
